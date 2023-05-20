78dbf5940c7f0ab220487bd111cf8e2990a94b15
hf04097 <hf04097@st.habib.edu.pk>
2023-05-20 22:50:12 +0500

    added application.yml, SmallWorldApplication and transaction entity

---
+++ coding_test/src/main/java/com/smallworld/model/Transaction.java
@@ -0,0 +1,20 @@
+package com.smallworld.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Transaction {
+
+    private Long mtn;
+    private Double amount;
+    private String senderFullName;
+    private Integer senderAge;
+    private String beneficiaryFullName;
+    private Integer beneficiaryAge;
+    private Integer issueId;
+    private Boolean issueSolved;
+    private String issueMessage;
+
+}
