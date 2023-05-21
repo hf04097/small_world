d378d3453737d90b302e51947430dc83a571287b
hf04097 <hf04097@st.habib.edu.pk>
2023-05-21 23:09:26 +0500

    specifying file location in application.yml

--- coding_test/src/main/java/com/smallworld/service/impl/TransactionServiceImpl.java
+++ coding_test/src/main/java/com/smallworld/service/impl/TransactionServiceImpl.java
@@ -7,6 +7,7 @@ import com.smallworld.model.Transaction;
 import com.smallworld.service.TransactionService;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -15,10 +16,13 @@ import java.util.List;
 @Service
 @Slf4j
 public class TransactionServiceImpl implements TransactionService {
+    @Value("${datasource.json.file-location}")
+    private String jsonFileLocation;
+
     private static List<Transaction> transactions;
 
     @PostConstruct
-    // Loading json file one and storing it in static variable
+        // Loading json file one and storing it in static variable
     void initTransaction() {
         getAllTransactions();
     }
@@ -32,8 +36,7 @@ public class TransactionServiceImpl implements TransactionService {
         ObjectMapper mapper = new ObjectMapper();
         try {
             log.info("loading file");
-            String filename = "src/main/resources/transactions.json";
-            File initialFile = new File(filename);
+            File initialFile = new File(jsonFileLocation);
             transactions = mapper.readValue(initialFile, new TypeReference<List<Transaction>>() {
             });
         } catch (Exception e) {
